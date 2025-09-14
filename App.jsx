fd62b7ebec3be9af2ffed4be32b22c1cb2c324ae
Yannawit-jommool <67024975@up.ac.th>
2025-09-14 16:33:52 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,24 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import Items from "./pages/Items";
+import Borrowing from "./pages/Borrowing";
+import History from "./pages/History";
+import "./style.css";
 
+export default function App() {
+  return (
+    <div className="layout">
+      <Sidebar />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/borrowing" element={<Borrowing />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
